f613c429ef9f774a23603c779ac5bf51da897d0a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-03-31 12:05:24 -0600

    feat: on update SW confirm to reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,14 +13,22 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-if (process.env.NODE_ENV === 'production') {
-  serviceWorkerRegistration.register();
+if (process.env.NODE_ENV === "production") {
+  serviceWorkerRegistration.register({
+    onUpdate(registration) {
+      // eslint-disable-next-line no-restricted-globals
+      if (confirm("There is a new version available, do you want to reload?")) {
+        registration.waiting?.postMessage({ type: "SKIP_WAITING" });
+        window.location.reload();
+      }
+    },
+  });
 } else {
   serviceWorkerRegistration.unregister();
 }
