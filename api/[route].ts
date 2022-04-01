88bb61da5f2ea65fb3dd5b6c4e4933a8681f969d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-04-01 15:58:43 -0600

    fix(api): pass the db as object

--- api/[route].ts
+++ api/[route].ts
@@ -1,39 +1,15 @@
-import fs from 'fs/promises';
-import os from 'os';
-import path from 'path';
 import jsonServer from 'json-server';
 
 import timestampMiddleware from '../timestampMiddleware.cjs';
+import db from '../db.json';
 
 const server = jsonServer.create();
+const router = jsonServer.router(db);
 const middleware = jsonServer.defaults({
   bodyParser: true,
   static: 'build',
 });
 
-async function exist(file: string): Promise<boolean> {
-  try {
-    await fs.access(file);
-
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-async function bootstrap() {
-  const db = path.resolve(process.cwd(), 'db.json');
-  const dest = path.join(os.tmpdir(), 'db.json');
-
-  if (!(await exist(dest))) {
-    fs.copyFile(db, dest);
-  }
-
-  const router = jsonServer.router(dest);
-
-  server.use('/api', ...middleware, timestampMiddleware, router);
-}
-
-void bootstrap();
+server.use('/api', ...middleware, timestampMiddleware, router);
 
 module.exports = server;
