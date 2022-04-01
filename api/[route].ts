71aead77add3969ea059118e6f4c51002039d8af
Jaime Leonardo Suncin Cruz <leosuncin@users.noreply.github.com>
2022-04-01 16:52:07 -0600

    fix(api): run json-server with vercel
    * Rewrite URL and define headers in vercel.json
    
    * Pass the db as object to use as in-memory

--- api/[route].ts
+++ api/[route].ts
@@ -1,9 +1,10 @@
 import jsonServer from 'json-server';
 
 import timestampMiddleware from '../timestampMiddleware.cjs';
+import db from '../db.json';
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(db);
 const middleware = jsonServer.defaults({
   bodyParser: true,
   static: 'build',
