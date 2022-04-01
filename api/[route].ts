2c63f27a995482d68e16cd03d276c495602839fb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-04-01 14:57:19 -0600

    fix: copy db.json to tmp

--- api/[route].ts
+++ api/[route].ts
@@ -1,14 +1,39 @@
+import fs from 'fs/promises';
+import os from 'os';
+import path from 'path';
 import jsonServer from 'json-server';
 
 import timestampMiddleware from '../timestampMiddleware.cjs';
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
 const middleware = jsonServer.defaults({
   bodyParser: true,
   static: 'build',
 });
 
-server.use('/api', ...middleware, timestampMiddleware, router);
+async function exist(file: string): Promise<boolean> {
+  try {
+    await fs.access(file);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function bootstrap() {
+  const db = path.resolve(process.cwd(), 'db.json');
+  const dest = path.join(os.tmpdir(), 'db.json');
+
+  if (!(await exist(dest))) {
+    fs.copyFile(db, dest);
+  }
+
+  const router = jsonServer.router(dest);
+
+  server.use('/api', ...middleware, timestampMiddleware, router);
+}
+
+void bootstrap();
 
 module.exports = server;
